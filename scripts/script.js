37b4c42a95766c8acca293a035b17e298c9aa07c
JustinAlkire <Justin.alkire@gmail.com>
2024-09-24 14:12:57 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,13 @@
-// $("body").css("background-color", "yellow");
-//when button clicked
-// IIFE Immediately invoked function expression
-$("#btnGetAnimal").on("click", function(){
-  //
-  
+$("#btnGetAnimal").on("click", function() {
   // Get username
   let bakerName = $("#bakerName").val();
-  // var variableName = ... let (const) 
-  
-  $("#greetingArea").text("Hi there, " + bakerName );
-  
-  //give them a personalized greeting
-  let randomAnimal = spiritAnimals[(Math.floor(Math.random() * spiritAnimals.length))];
-  $("#greetingArea").text("Your spirit animal is: " + randomAnimal)
-})
 
-// next, also tell spirit animal
-let spiritAnimals = ["🐈","🦆","👾"]
+  // Generate a random spirit animal
+  let randomAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
+
+  // Update the greeting area with both the greeting and the spirit animal
+  $("#greetingArea").text("Hi there, " + bakerName + ". Your spirit animal is: " + randomAnimal);
+});
+
+// List of spirit animals
+let spiritAnimals = ["🐈", "🦆", "👾"];
